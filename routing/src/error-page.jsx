ec11d941ecdc21a3362d1e8fac4173295024fac1
LutLatAung <u6511163@au.edu>
2024-08-16 14:01:13 +0700

    .

--- routing/src/error-page.jsx
+++ routing/src/error-page.jsx
@@ -11,6 +11,7 @@ export default function ErrorPage() {
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+      <button onClick={() => window.location.href = "/routing/"}>Go to Main Page</button>
     </div>
   );
 }
